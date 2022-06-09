0400f878ba22332051ef53c13226d61f15699751
pazMenachem <paz.menach@gmail.com>
2022-06-09 18:16:39 +0300

    update by Mario

--- Truck.h
+++ Truck.h
@@ -5,25 +5,29 @@
 #include <vector>
 #include "Vehicle.h"
 
+using routesVec = vector <pair<string, pair<double, int> > >;
+
 class Truck: public Vehicle{
 private:
     int _crates;
-    vector < pair < string, pair<double, int> > > routs; // vactor < destination name: (time,crates) >
+    routesVec _routes; // vactor < destination name: (time,crates) >
     int _nextIndex;
 public:
     Truck(string &name, const Point& location, string vType): Vehicle(name,location,vType), _nextIndex(0) {Vehicle::setState("Moving");};
 
     int getCrates() const { return _crates; }
 
-    void setRouts(const vector<pair<string, pair<double, int>>> &routs);
+    void setRouts(const routesVec &routes);
 
     void setCrates(int crates) {
         _crates = crates;
     }
     void getStatus();
 
-private:
-    public:
+    bool checkCops();
+
+    void robbed();
+
 };
 
 #endif //GAMESIMULATOR_TRUCK_H
